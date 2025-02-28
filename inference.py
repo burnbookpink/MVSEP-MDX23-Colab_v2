4c90b141ce5083f1dacbe0dd021ccadd268a56a1
Elena <62360146+stellarcore@users.noreply.github.com>
2025-02-28 12:33:50 +0200

    Add files via upload

--- inference.py
+++ inference.py
@@ -556,7 +556,7 @@ class EnsembleDemucsMDXMusicSeparationModel:
                     vocals_model_outputs.append(vocals_scnet)
                     if not options['large_gpu']:
                         print(f'Unloading {model_name} from memory')
-                        self.model_scnet.cpu()
+                        del self.model_scnet.cpu()
                      del sources_scnet
                      torch.cuda.empty_cache()
                      weights.append(options.get(f"weight_{model_name}"))
